c79c39a59cba6bfd8ad1158cf4b6151bab0a4af1
naveen <naveenksb1198@gmail.com>
2023-11-28 12:05:58 +0530

    Update the class By TL

--- src/test/java/group/TlUpdateTest.java
+++ src/test/java/group/TlUpdateTest.java
@@ -4,6 +4,9 @@ public class TlUpdateTest {
 	
    public static void main(String[] args) {
 	System.out.println("hello script is updated");
+	System.out.println("hello script is updated");
+	System.out.println("hello script is updated");
+	
 }
 
 }
